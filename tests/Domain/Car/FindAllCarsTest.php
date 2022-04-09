90a0f56d1355d21ed1ccf76eb086d5d675d12dc0
Jorge Oscar Gianotti <jorge.oscar.gianotti@gmail.com>
2022-04-09 11:45:54 -0300

    test: add all cars finder test

---
+++ tests/Domain/Car/FindAllCarsTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Tests\Domain\Car;
+
+use App\Domain\Car\AllCarsFinder;
+use App\Domain\Car\CarRepository;
+use Mockery;
+use Mockery\MockInterface;
+use PHPUnit\Framework\TestCase;
+
+class FindAllCarsTest extends TestCase
+{
+    private MockInterface|CarRepository $carRepository;
+
+    /** @doesNotPerformAssertions */
+    public function testItShouldFindAllCars(): void
+    {
+        $limit = 10;
+        $offset = 0;
+
+        $this->carRepository
+            ->shouldReceive('all')
+            ->with($limit, $offset)
+            ->once();
+
+        (new AllCarsFinder($this->carRepository))($limit, $offset);
+    }
+
+    protected function setUp(): void
+    {
+        $this->carRepository = Mockery::mock(CarRepository::class);
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        Mockery::close();
+    }
+}
