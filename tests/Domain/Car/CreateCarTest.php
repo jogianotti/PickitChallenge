bbcaa16f9f6a4c391c9f54e4afba09fd2801bf3c
Jorge Oscar Gianotti <jorge.oscar.gianotti@gmail.com>
2022-04-09 11:05:58 -0300

    test: add car creator test

---
+++ tests/Domain/Car/CreateCarTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Tests\Domain\Car;
+
+use App\Domain\Car\Car;
+use App\Domain\Car\CarCreator;
+use App\Domain\Car\CarRepository;
+use App\Domain\Car\Patent;
+use App\Domain\Shared\Uuid;
+use Hamcrest\Matchers;
+use Mockery;
+use PHPUnit\Framework\TestCase;
+
+class CreateCarTest extends TestCase
+{
+    /** @doesNotPerformAssertions */
+    public function testItShouldCreateACar(): void
+    {
+        $uuid = new Uuid('9239c274-3240-4730-9eac-1c3ec595c346');
+        $brand = 'Renault';
+        $model = 'Duster';
+        $year = 2021;
+        $patent = new Patent('YR578ER');
+        $color = 'Naranja';
+
+        $car = Car::create(
+            uuid: $uuid,
+            brand: $brand,
+            model: $model,
+            year: $year,
+            patent: $patent,
+            color: $color
+        );
+
+        $this->carRepository
+            ->shouldReceive('save')
+            ->with(Matchers::equalTo($car))
+            ->once();
+
+        (new CarCreator($this->carRepository))($uuid, $brand, $model, $year, $patent, $color);
+    }
+
+    protected function setUp(): void
+    {
+        $this->carRepository = Mockery::mock(CarRepository::class);
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        Mockery::close();
+    }
+}
