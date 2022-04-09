c099888f5eed96a28c3c89230584f13a82f02580
Jorge Oscar Gianotti <jorge.oscar.gianotti@gmail.com>
2022-04-09 14:31:19 -0300

    test: add car finder test

---
+++ tests/Domain/Car/FindCarTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Tests\Domain\Car;
+
+use App\Domain\Car\CarFinder;
+use App\Domain\Car\CarRepository;
+use App\Domain\Shared\Uuid;
+use Mockery;
+use Mockery\MockInterface;
+use PHPUnit\Framework\TestCase;
+
+class FindCarTest extends TestCase
+{
+    private MockInterface|CarRepository $carRepository;
+
+    /** @doesNotPerformAssertions */
+    public function testItShouldFindAllCars(): void
+    {
+        $uuid = new Uuid('d44e052f-2d9d-4d97-b377-16b8ad2421a1');
+
+        $this->carRepository
+            ->shouldReceive('one')
+            ->with($uuid->value())
+            ->once();
+
+        (new CarFinder($this->carRepository))($uuid);
+    }
+
+    protected function setUp(): void
+    {
+        $this->carRepository = Mockery::mock(CarRepository::class);
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        Mockery::close();
+    }
+}
