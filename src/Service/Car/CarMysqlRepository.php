4b18fc053dfc6dadd7460a801be5f287eb9a0dcb
Jorge Oscar Gianotti <jorge.oscar.gianotti@gmail.com>
2022-04-10 10:43:25 -0300

    feat: implements car mysql repository

--- src/Service/Car/CarMysqlRepository.php
+++ src/Service/Car/CarMysqlRepository.php
@@ -4,30 +4,41 @@ namespace App\Service\Car;
 
 use App\Domain\Car\Car;
 use App\Domain\Car\CarRepository;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
 
-class CarMysqlRepository implements CarRepository
+/**
+ * @method Car|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Car|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Car[]    findAll()
+ * @method Car[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class CarMysqlRepository extends ServiceEntityRepository implements CarRepository
 {
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Car::class);
+    }
+
     public function save(Car $car): void
     {
-        // TODO: Implement save() method.
+        $this->_em->persist($car);
+        $this->_em->flush();
     }
 
     public function all(int $limit, int $offset): array
     {
-        // TODO: Implement all() method.
-
-        return [];
+        return $this->findBy(criteria: [], limit: $limit, offset: $offset);
     }
 
     public function one(string $id): ?Car
     {
-        // TODO: Implement one() method.
-
-        return null;
+        return $this->find(id: $id);
     }
 
-    public function delete(string $id): void
+    public function delete(Car $car): void
     {
-        // TODO: Implement delete() method.
+        $this->_em->remove($car);
+        $this->_em->flush();
     }
 }
